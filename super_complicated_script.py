92887937c1be4aabe9f413e9ca8cb991b2452209
iejMac <kilianmaciej6@gmail.com>
2022-12-03 22:51:20 +0000

    merge this please

---
+++ super_complicated_script.py
@@ -0,0 +1,8 @@
+import random
+
+# Set the number of sides on the die
+num_sides = 6
+
+# Roll the die and print the result to the console
+roll_result = random.randint(1, num_sides)
+print(f"You rolled a {roll_result}")
