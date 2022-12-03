823a222c0ade913ba9e069071178c3016efa8e87
iejMac <kilianmaciej6@gmail.com>
2022-12-03 20:43:09 +0000

    test

--- review.py
+++ review.py
@@ -9,7 +9,6 @@ def get_review():
   with open(github_env, "r") as f:
     variables = dict([line.split("=") for line in f.readlines()])
   pr_link = variables["LINK"]
-  openai.api_key = variables["OPENAI_API_KEY"]
 
   request_link = "https://patch-diff.githubusercontent.com/raw/" + pr_link[len("https://github.com/"):] + ".patch"
   patch = requests.get(request_link).text
@@ -18,7 +17,21 @@ def get_review():
   prompt = patch[:2048 - len(question)] + question
 
   prompt = "test"
+  headers = {
+    # Already added when you pass json=
+    # 'Content-Type': 'application/json',
+    'Authorization': f'Bearer {variables["OPENAI_API_KEY"]}',
+  }
+  json_data = {
+    'model': 'text-davinci-003',
+    'prompt': 'Say this is a test',
+    'temperature': 0,
+    'max_tokens': 7,
+  }
 
+  response = requests.post('https://api.openai.com/v1/completions', headers=headers, json=json_data)
+
+  '''
   response = openai.Completion.create(
     engine="text-ada-001",
     prompt=prompt,
@@ -28,6 +41,7 @@ def get_review():
     frequency_penalty=0.0,
     presence_penalty=0.0
   )
+  '''
   review = response['choices'][0]['text']
   return review
 
