24032b35b0a478a6f243992b17af853ae23d76ed
iejMac <kilianmaciej6@gmail.com>
2022-12-03 08:46:14 -0800

    get number

--- review.py
+++ review.py
@@ -5,17 +5,7 @@ import openai
 
 
 def get_review():
-  # Get the path to the GITHUB_EVENT_PATH environment variable
-  event_path = os.environ["GITHUB_EVENT_PATH"]
-
-  # Load the event data from the GITHUB_EVENT_PATH file
-  with open(event_path, "r") as f:
-      event_data = json.load(f)
-
-  # Access the html_url property of the pull_request object to get the link to the PR
-  pr_link = event_data["pull_request"]["html_url"]
-
-
+  pr_link = None
   review = f"Horrible code, please stop, {pr_link}"
   return review
 
