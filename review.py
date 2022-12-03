b8f5ab702c2e07d1fd4f3534146fa59c763bf12a
iejMac <kilianmaciej6@gmail.com>
2022-12-03 10:38:24 -0800

    first try

--- review.py
+++ review.py
@@ -8,14 +8,23 @@ def get_review():
   github_env = os.getenv("GITHUB_ENV")
   with open(github_env, "r") as f:
     variables = dict([line.split("=") for line in f.readlines()])
-
   pr_link = variables["LINK"]
-
+  openai.api_key = variables["OPENAI_API_KEY"]
   request_link = "https://patch-diff.githubusercontent.com/raw/" + pr_link[len("https://github.com/"):] + ".patch"
-
   patch = requests.get(request_link).text
-  # review = f"Horrible code, please stop {pr_link} {patch}"
-  review = patch[:10]
+
+  prompt = patch
+
+  response = openai.Completion.create(
+    engine="text-ada-001",
+    prompt=prompt,
+    temperature=0.5,
+    max_tokens=256,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0
+  )
+  review = response['choices'][0]['text']
   return review
 
 if __name__ == "__main__":
