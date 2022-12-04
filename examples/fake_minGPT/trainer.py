db420fbd8a0becf50b9b2bee8abb660cf145ea9b
iejMac <kilianmaciej6@gmail.com>
2022-12-03 20:16:17 -0800

    example: minGPT amp training

--- examples/fake_minGPT/trainer.py
+++ examples/fake_minGPT/trainer.py
@@ -3,107 +3,132 @@ Simple training loop; Boilerplate that could apply to any arbitrary neural netwo
 so nothing in this file really has anything to do with GPT specifically.
 """
 
-import time
-from collections import defaultdict
+import math
+import logging
+
+from tqdm import tqdm
+import numpy as np
 
 import torch
+import torch.optim as optim
+import torch.cuda.amp as amp
+from torch.optim.lr_scheduler import LambdaLR
 from torch.utils.data.dataloader import DataLoader
-from mingpt.utils import CfgNode as CN
+
+logger = logging.getLogger(__name__)
+
+class TrainerConfig:
+    # optimization parameters
+    max_epochs = 10
+    batch_size = 64
+    learning_rate = 3e-4
+    betas = (0.9, 0.95)
+    grad_norm_clip = 1.0
+    weight_decay = 0.1 # only applied on matmul weights
+    # learning rate decay params: linear warmup followed by cosine decay to 10% of original
+    lr_decay = False
+    warmup_tokens = 375e6 # these two numbers come from the GPT-3 paper, but may not be good defaults elsewhere
+    final_tokens = 260e9 # (at what point we reach 10% of original LR)
+    # checkpoint settings
+    ckpt_path = None
+    num_workers = 0 # for DataLoader
+
+    def __init__(self, **kwargs):
+        for k,v in kwargs.items():
+            setattr(self, k, v)
 
 class Trainer:
 
-    @staticmethod
-    def get_default_config():
-        C = CN()
-        # device to train on
-        C.device = 'auto'
-        # dataloder parameters
-        C.num_workers = 4
-        # optimizer parameters
-        C.max_iters = None
-        C.batch_size = 64
-        C.learning_rate = 3e-4
-        C.betas = (0.9, 0.95)
-        C.weight_decay = 0.1 # only applied on matmul weights
-        C.grad_norm_clip = 1.0
-        return C
-
-    def __init__(self, config, model, train_dataset):
-        self.config = config
+    def __init__(self, model, train_dataset, test_dataset, config):
         self.model = model
-        self.optimizer = None
         self.train_dataset = train_dataset
-        self.callbacks = defaultdict(list)
+        self.test_dataset = test_dataset
+        self.config = config
 
-        # determine the device we'll train on
-        if config.device == 'auto':
-            self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        else:
-            self.device = config.device
-        self.model = self.model.to(self.device)
-        print("running on device", self.device)
+        # take over whatever gpus are on the system
+        self.device = 'cpu'
+        if torch.cuda.is_available():
+            self.device = torch.cuda.current_device()
+            self.model = torch.nn.DataParallel(self.model).to(self.device)
 
-        # variables that will be assigned to trainer class later for logging and etc
-        self.iter_num = 0
-        self.iter_time = 0.0
-        self.iter_dt = 0.0
+    def save_checkpoint(self):
+        if self.config.ckpt_path is not None:
+            ckpt_model = self.model.module if hasattr(self.model, "module") else self.model
+            logger.info("saving %s", self.config.ckpt_path)
+            torch.save(ckpt_model.state_dict(), self.config.ckpt_path)
 
-    def add_callback(self, onevent: str, callback):
-        self.callbacks[onevent].append(callback)
-
-    def set_callback(self, onevent: str, callback):
-        self.callbacks[onevent] = [callback]
-
-    def trigger_callbacks(self, onevent: str):
-        for callback in self.callbacks.get(onevent, []):
-            callback(self)
-
-    def run(self):
+    def train(self):
         model, config = self.model, self.config
 
-        # setup the optimizer
-        self.optimizer = model.configure_optimizers(config)
+        # create the optimizer
+        no_decay = ["bias", "LayerNorm.weight"]
+        params_decay = [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)]
+        params_nodecay = [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)]
+        optim_groups = [
+            {"params": params_decay, "weight_decay": config.weight_decay},
+            {"params": params_nodecay, "weight_decay": 0.0},
+        ]
+        optimizer = optim.AdamW(optim_groups, lr=config.learning_rate, betas=config.betas)
+        scaler = amp.GradScaler()
 
-        # setup the dataloader
-        train_loader = DataLoader(
-            self.train_dataset,
-            sampler=torch.utils.data.RandomSampler(self.train_dataset, replacement=True, num_samples=int(1e10)),
-            shuffle=False,
-            pin_memory=True,
-            batch_size=config.batch_size,
-            num_workers=config.num_workers,
-        )
+        def run_epoch(split):
+            is_train = split == 'train'
+            model.train(is_train)
+            data = self.train_dataset if is_train else self.test_dataset
+            loader = DataLoader(data, batch_size=config.batch_size, num_workers=config.num_workers)
 
-        model.train()
-        self.iter_num = 0
-        self.iter_time = time.time()
-        data_iter = iter(train_loader)
-        while True:
+            losses = []
+            pbar = tqdm(enumerate(loader), total=len(loader)) if is_train else enumerate(loader)
+            for it, (x, y) in pbar:
 
-            # fetch the next batch (x, y) and re-init iterator if needed
-            try:
-                batch = next(data_iter)
-            except StopIteration:
-                data_iter = iter(train_loader)
-                batch = next(data_iter)
-            batch = [t.to(self.device) for t in batch]
-            x, y = batch
+                # place data on the correct device
+                x = x.to(self.device)
+                y = y.to(self.device)
 
-            # forward the model
-            logits, self.loss = model(x, y)
+                # forward the model
+                with torch.set_grad_enabled(is_train):
+                    with amp.autocast():
+                        logits, loss = model(x, y)
+                        loss = loss.mean() # collapse all losses if they are scattered on multiple gpus
+                    losses.append(loss.item())
 
-            # backprop and update the parameters
-            model.zero_grad(set_to_none=True)
-            self.loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), config.grad_norm_clip)
-            self.optimizer.step()
+                if is_train:
 
-            self.trigger_callbacks('on_batch_end')
-            self.iter_num += 1
-            tnow = time.time()
-            self.iter_dt = tnow - self.iter_time
-            self.iter_time = tnow
+                    # backprop and update the parameters
+                    model.zero_grad()
+                    scaler.scale(loss).backward()
+                    scaler.unscale_(optimizer)
+                    torch.nn.utils.clip_grad_norm_(model.parameters(), config.grad_norm_clip)
+                    scaler.step(optimizer)
+                    scaler.update()
 
-            # termination conditions
-            if config.max_iters is not None and self.iter_num >= config.max_iters:
-                break
+                    # decay the learning rate based on our progress
+                    if config.lr_decay:
+                        self.tokens += (y >= 0).sum() # number of tokens processed this step (i.e. label is not -100)
+                        if self.tokens < config.warmup_tokens:
+                            # linear warmup
+                            lr_mult = float(self.tokens) / float(max(1, config.warmup_tokens))
+                        else:
+                            # cosine learning rate decay
+                            progress = float(self.tokens - config.warmup_tokens) / float(max(1, config.final_tokens - config.warmup_tokens))
+                            lr_mult = max(0.1, 0.5 * (1.0 + math.cos(math.pi * progress)))
+                        lr = config.learning_rate * lr_mult
+                        for param_group in optimizer.param_groups:
+                            param_group['lr'] = lr
+                    else:
+                        lr = config.learning_rate
+
+                    # report progress
+                    pbar.set_description(f"epoch {epoch+1} iter {it}: train loss {loss.item():.5f}. lr {lr:e}")
+
+            if not is_train:
+                logger.info("test loss: %f", np.mean(losses))
+
+        self.tokens = 0 # counter used for learning rate decay
+        for epoch in range(config.max_epochs):
+
+            run_epoch('train')
+            if self.test_dataset is not None:
+                run_epoch('test')
+
+            self.save_checkpoint()
--- examples/fake_minGPT/utils.py
+++ examples/fake_minGPT/utils.py
@@ -1,14 +1,9 @@
-
-import os
-import sys
-import json
 import random
-from ast import literal_eval
-
 import numpy as np
 import torch
-
-# -----------------------------------------------------------------------------
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.cuda.amp import autocast
 
 def set_seed(seed):
     random.seed(seed)
@@ -16,88 +11,39 @@ def set_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
-def setup_logging(config):
-    """ monotonous bookkeeping """
-    work_dir = config.system.work_dir
-    # create the work directory if it doesn't already exist
-    os.makedirs(work_dir, exist_ok=True)
-    # log the args (if any)
-    with open(os.path.join(work_dir, 'args.txt'), 'w') as f:
-        f.write(' '.join(sys.argv))
-    # log the config itself
-    with open(os.path.join(work_dir, 'config.json'), 'w') as f:
-        f.write(json.dumps(config.to_dict(), indent=4))
+def top_k_logits(logits, k):
+    v, ix = torch.topk(logits, k)
+    out = logits.clone()
+    out[out < v[:, [-1]]] = 1e-10
+    return out
 
-class CfgNode:
-    """ a lightweight configuration class inspired by yacs """
-    # TODO: convert to subclass from a dict like in yacs?
-    # TODO: implement freezing to prevent shooting of own foot
-    # TODO: additional existence/override checks when reading/writing params?
-
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
-
-    def __str__(self):
-        return self._str_helper(0)
-
-    def _str_helper(self, indent):
-        """ need to have a helper to support nested indentation for pretty printing """
-        parts = []
-        for k, v in self.__dict__.items():
-            if isinstance(v, CfgNode):
-                parts.append("%s:\n" % k)
-                parts.append(v._str_helper(indent + 1))
+@torch.no_grad()
+def sample(model, x, steps, temperature=1.0, sample=False, top_k=None):
+    """
+    take a conditioning sequence of indices in x (of shape (b,t)) and predict the next token in
+    the sequence, feeding the predictions back into the model each time. Clearly the sampling
+    has quadratic complexity unlike an RNN that is only linear, and has a finite context window
+    of block_size, unlike an RNN that has an infinite context window.
+    """
+    block_size = model.get_block_size()
+    model.eval()
+    for k in range(steps):
+        x_cond = x if x.size(1) <= block_size else x[:, -block_size:] # crop context if needed
+        with autocast():
+            logits, _ = model(x_cond)
+            # pluck the logits at the final step and scale by temperature
+            logits = logits[:, -1, :] / temperature
+            # optionally crop probabilities to only the top k options
+            if top_k is not None:
+                logits = top_k_logits(logits, top_k)
+            # apply softmax to convert to probabilities
+            probs = F.softmax(logits, dim=-1)
+            # sample from the distribution or take the most likely
+            if sample:
+                ix = torch.multinomial(probs, num_samples=1)
             else:
-                parts.append("%s: %s\n" % (k, v))
-        parts = [' ' * (indent * 4) + p for p in parts]
-        return "".join(parts)
+                _, ix = torch.topk(probs, k=1, dim=-1)
+        # append to the sequence and continue
+        x = torch.cat((x, ix), dim=1)
 
-    def to_dict(self):
-        """ return a dict representation of the config """
-        return { k: v.to_dict() if isinstance(v, CfgNode) else v for k, v in self.__dict__.items() }
-
-    def merge_from_dict(self, d):
-        self.__dict__.update(d)
-
-    def merge_from_args(self, args):
-        """
-        update the configuration from a list of strings that is expected
-        to come from the command line, i.e. sys.argv[1:].
-
-        The arguments are expected to be in the form of `--arg=value`, and
-        the arg can use . to denote nested sub-attributes. Example:
-
-        --model.n_layer=10 --trainer.batch_size=32
-        """
-        for arg in args:
-
-            keyval = arg.split('=')
-            assert len(keyval) == 2, "expecting each override arg to be of form --arg=value, got %s" % arg
-            key, val = keyval # unpack
-
-            # first translate val into a python object
-            try:
-                val = literal_eval(val)
-                """
-                need some explanation here.
-                - if val is simply a string, literal_eval will throw a ValueError
-                - if val represents a thing (like an 3, 3.14, [1,2,3], False, None, etc.) it will get created
-                """
-            except ValueError:
-                pass
-
-            # find the appropriate object to insert the attribute into
-            assert key[:2] == '--'
-            key = key[2:] # strip the '--'
-            keys = key.split('.')
-            obj = self
-            for k in keys[:-1]:
-                obj = getattr(obj, k)
-            leaf_key = keys[-1]
-
-            # ensure that this attribute exists
-            assert hasattr(obj, leaf_key), f"{key} is not an attribute that exists in the config"
-
-            # overwrite the attribute
-            print("command line overwriting config attribute %s with %s" % (key, val))
-            setattr(obj, leaf_key, val)
+    return x
