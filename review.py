0fdc1060946892b5adc53d104baab878287160d4
Sam Hill <samhilll@Sams-MacBook-Pro.local>
2022-12-12 20:48:07 -0500

    update to fix permissions issues and clean up code

--- review.py
+++ review.py
@@ -1,59 +1,68 @@
 import os
 import requests
 import json
+import subprocess
 
 import openai
 
 
-WHITELIST = ["iejMac"] # move this to github actions (probably some 'uses' I don't know about
-
-
 def get_review():
-  github_env = os.getenv("GITHUB_ENV")
-  with open(github_env, "r") as f:
-    variables = dict([line.split("=") for line in f.read().splitlines()])
+  pr_link = os.getenv("LINK")
+  openai.api_key = os.getenv("OPENAI_API_KEY")
+  ACCESS_TOKEN = os.getenv("GITHUB_TOKEN")
+  GIT_COMMIT_HASH = os.getenv("GIT_COMMIT_HASH")
+  PR_TITLE = os.getenv("PR_TITLE")
+  PR_BODY = os.getenv("PR_BODY")
 
-  if variables["GITHUB_ACTOR"] not in WHITELIST: # only run review for whitelisted users
-      return
+  headers = {
+    "Accept": "application/vnd.github.v3.patch",
+    "authorization": f"Bearer {ACCESS_TOKEN}"
+  }
+  
+  fetch_result = subprocess.check_output(["git", "fetch", "origin", "master"])
+  print("fetch result")
+  print(fetch_result)
+  diff_result = str(subprocess.check_output(["git", "diff", "origin/master"]))
+  print("diff result")
+  print(diff_result)
+  
 
-  pr_link = variables["LINK"]
-  openai.api_key = variables["OPENAI_API_KEY"]
 
-  request_link = "https://patch-diff.githubusercontent.com/raw/" + pr_link[len("https://github.com/"):] + ".patch"
-  patch = requests.get(request_link).text
+  intro = f"\n Here is a pull request. Please assume you are a reviewer of this PR. First I will tell you the title and body of the PR. \n"
+  pr_title = f"The title is {PR_TITLE}.\n"
+  pr_body = f"The body is {PR_BODY}.\n"
+  question = "Can you tell me the problems with the following pull request and provide specific suggestions to improve it?"
+  pr_diff = f"Here's the diff of what changed in this PR: {diff_result}"
+  prompt = intro + pr_title + pr_body + question + pr_diff
 
-  question = "\n Can you summarize this GitHub Pull Request for me and suggest possible improvements?"
-  prompt = patch[:4096 - len(question.split(" "))] + question
+  print(f"prompt: {prompt}")
 
   # model = "text-ada-001"
   model = "text-davinci-003"
   response = openai.Completion.create(
     engine=model,
     prompt=prompt,
-    temperature=0.9,
-    max_tokens=512, # TODO: need to find a dynamic way of setting this according to the prompt
+    temperature=0.5,
+    max_tokens=256,
     top_p=1.0,
     frequency_penalty=0.0,
     presence_penalty=0.0
   )
   review = response['choices'][0]['text']
 
-  ACCESS_TOKEN = variables["GITHUB_TOKEN"]
-  headers = {
-    'Accept': 'application/vnd.github+json',
-    'Authorization': f'Bearer {ACCESS_TOKEN}',
-    'Content-Type': 'application/x-www-form-urlencoded',
-  }
+  
 
-  data = {"body": review}
+  data = {"body": review, "commit_id": GIT_COMMIT_HASH, "event": "COMMENT"}
   data = json.dumps(data)
+  print(f"openAI response {data}")
 
 
   OWNER = pr_link.split("/")[-4]
   REPO = pr_link.split("/")[-3]
   PR_NUMBER = pr_link.split("/")[-1]
 
-  response = requests.post(f'https://api.github.com/repos/{OWNER}/{REPO}/issues/{PR_NUMBER}/comments', headers=headers, data=data)
+  # https://api.github.com/repos/OWNER/REPO/pulls/PULL_NUMBER/reviews
+  response = requests.post(f'https://api.github.com/repos/{OWNER}/{REPO}/pulls/{PR_NUMBER}/reviews', headers=headers, data=data)
   print(response.json())
 
 
